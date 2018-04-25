1adc43f493a3c18265e533e413d509123de36a82
doub <doub_9527@163.com>
2018-04-25 19:43:06 +0800

    Update FootController.java
    Signed-off-by: doub <doub_9527@163.com>

--- src/test/java/com/jk/SpringbootHouseFrontApplicationTests.java
+++ src/test/java/com/jk/SpringbootHouseFrontApplicationTests.java
@@ -1,5 +1,6 @@
 package com.jk;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootHouseFrontApplicationTests {
 
+
+
 	@Test
 	public void contextLoads() {
+
+		 Assert.assertEquals("1","1");
 	}
 
 }
